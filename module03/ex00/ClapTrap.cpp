3d9c1b93052d33018241770947f34d2ed3cbb8fe
Othman Mallah <othmanmallah15@gmail.com>
2022-08-26 16:29:49 +0100

    module03 coming

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -3,14 +3,10 @@
 ClapTrap::ClapTrap( std::string name)
 {
     this->name = name;
-    std::cout << "Constructore called " << std::endl;
-}
-
-ClapTrap::ClapTrap()
-{
     this->Hit_points = 10;
     this->Energy_points = 10;
     this->Attack_damage = 0; 
+    std::cout << "Constructore called " << std::endl;
 }
 
 ClapTrap::~ClapTrap()
@@ -24,26 +20,26 @@ void    ClapTrap::attack( const std::string& target)
         std::cout << "ClapTrap can't do nothing!" << std::endl;
     else
     {
-        this->Attack_damage = this->Hit_points;
-        this->Hit_points = 0;
+        
         this->Energy_points--;
-       std::cout << "ClapTrap" << this->name << "attacks"  << target << ", causing " << this->Attack_damage << "points of damage!" << std::endl;
+        std::cout << "ClapTrap " << this->name << " attacks "  << target << "v, causing " << this->Attack_damage << " points of damage!" << std::endl;
     }
 }
 
 void    ClapTrap::takeDamage( unsigned int amount )
 {
-    std::cout << "ClapTrap" << this->name << "attacks"  << amount << ", causing " << this->Attack_damage << "points of damage!" << std::endl;
+    this->Hit_points = this->Hit_points - amount;
+    std::cout << "ClapTrap " << this->name << " takeDmage "  << amount << std::endl;
 }
 
 void    ClapTrap::beRepaired( unsigned int amount)
 {
-    if (this->Energy_points == 0)
+    if (this->Hit_points == 0 || this->Energy_points == 0)
         std::cout << "ClapTrap can't do nothing!" << std::endl;
     else
     {
-        this->Hit_points = amount;
+        this->Hit_points += amount;
         this->Energy_points--;
-        std::cout << "ClapTrap" << this->name << "attacks"  << amount << ", causing " << this->Attack_damage << "points of damage!" << std::endl;
+        std::cout << "ClapTrap " << this->name << " Repaired "  << amount << ", ADD in Hits points" <<  std::endl;
     }
 }
--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: otmallah <otmallah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 20:12:45 by otmallah          #+#    #+#             */
-/*   Updated: 2022/08/25 20:13:22 by otmallah         ###   ########.fr       */
+/*   Updated: 2022/08/26 16:03:52 by otmallah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 
 int main()
 {
-    ClapTrap ClapTrack;
-    ClapTrack("POOK");
-    ClapTrack.attack("giik");
-    ClapTrack.beRepaired(20);
-    
-    
+    ClapTrap claptrack = ClapTrap(" LEGEND ");
+
+    claptrack.attack("POOK");
+    claptrack.takeDamage(10);
+    claptrack.beRepaired(200);
 }
