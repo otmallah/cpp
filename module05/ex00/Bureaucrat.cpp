d585c77dd4b24351dfb939ca8998a55d6f20de54
Othman Mallah <othmanmallah15@gmail.com>
2022-09-01 17:39:55 +0100

    puush

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: otmallah <otmallah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 19:46:19 by otmallah          #+#    #+#             */
-/*   Updated: 2022/08/31 13:19:51 by otmallah         ###   ########.fr       */
+/*   Updated: 2022/09/01 14:56:02 by otmallah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,17 @@ Buraucrat::Buraucrat(std::string name1, int numberGrade):name(name1)
         this->grade = numberGrade;
     }
     std::cout << "Buraucrat Constructor called\n" << std::endl;
+}
+
+Buraucrat::Buraucrat(const Buraucrat& new_obj):name(new_obj.name)
+{
+    this->grade = new_obj.grade;
+}
+
+Buraucrat & Buraucrat::operator = (const Buraucrat & old_obj)
+{
+    this->grade = old_obj.grade;
+    return *this;
 }
 
 std::ostream & operator << (std::ostream &out ,Buraucrat const & old_obj)
